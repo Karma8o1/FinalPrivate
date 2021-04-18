8b3bb3fa423e736dca78f3c685e21dca27e5fa59
Nouma <Noumanfaiz.369@gmail.com>
2021-04-18 12:28:02 +0500

    FINALIZING PROJECT

--- app/src/main/java/com/example/stc/Pages.java
+++ app/src/main/java/com/example/stc/Pages.java
@@ -14,6 +14,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.fragment.app.FragmentTransaction;
 
 import com.google.android.material.navigation.NavigationView;
 import com.google.firebase.auth.FirebaseAuth;
@@ -56,6 +57,11 @@ public class Pages extends AppCompatActivity implements View.OnClickListener, Na
 
             }
         });
+
+        Profile profile = new Profile();
+        FragmentTransaction transaction2 = getSupportFragmentManager().beginTransaction();
+        transaction2.replace(R.id.container,profile);
+        transaction2.commit();
 
         setContentView(R.layout.activity_pages);toolbar = (Toolbar) findViewById(R.id.my_toolbar);
         setSupportActionBar(toolbar);
@@ -107,15 +113,33 @@ public class Pages extends AppCompatActivity implements View.OnClickListener, Na
     @Override
     public boolean onNavigationItemSelected(@NonNull MenuItem item) {
         switch(item.getItemId()){
-            case R.id.nav_home:
-                break;
             case R.id.Universities:
+                uni university = new uni();
+                FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+                transaction.replace(R.id.container,university);
+                transaction.commit();
+                toolbar.setTitle("UNIVERSITIES");
                 break;
             case R.id.Rental:
+                rentals rent = new rentals();
+                FragmentTransaction transaction1 = getSupportFragmentManager().beginTransaction();
+                transaction1.replace(R.id.container,rent);
+                transaction1.commit();
+                toolbar.setTitle("RENTAL SERVICES");
                 break;
             case R.id.profile:
+                Profile profile = new Profile();
+                FragmentTransaction transaction2 = getSupportFragmentManager().beginTransaction();
+                transaction2.replace(R.id.container,profile);
+                transaction2.commit();
+                toolbar.setTitle("PROFILE");
                 break;
             case R.id.settings:
+                settings set = new settings();
+                FragmentTransaction transaction3 = getSupportFragmentManager().beginTransaction();
+                transaction3.replace(R.id.container,set);
+                transaction3.commit();
+                toolbar.setTitle("SETTINGS");
                 break;
             case R.id.logout:
             logout();
