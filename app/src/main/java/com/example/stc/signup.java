927c577554a21520cad4b961fb637d79d2c07710
Nouma <Noumanfaiz.369@gmail.com>
2021-04-20 15:50:44 +0500

    FINALIZING PROJECT

--- app/src/main/java/com/example/stc/signup.java
+++ app/src/main/java/com/example/stc/signup.java
@@ -1,16 +1,20 @@
 package com.example.stc;
 
+import android.app.DatePickerDialog;
 import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.DialogFragment;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
@@ -20,13 +24,17 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.iid.FirebaseInstanceId;
 
+import java.text.DateFormat;
+import java.util.Calendar;
 import java.util.HashMap;
 
-public class signup extends AppCompatActivity implements View.OnClickListener{
+public class signup extends AppCompatActivity implements View.OnClickListener, DatePickerDialog.OnDateSetListener {
     private FirebaseAuth firebaseAuth;
     private ProgressDialog progressDialog;
     private DatabaseReference mDatabase;
     private Button register;
+    TextView DOB;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -34,6 +42,8 @@ public class signup extends AppCompatActivity implements View.OnClickListener{
         firebaseAuth = FirebaseAuth.getInstance();
         progressDialog = new ProgressDialog(this);
         register = (Button)findViewById(R.id.register);
+        DOB = (TextView) findViewById(R.id.DOB);
+        DOB.setOnClickListener(this);
         register.setOnClickListener(this);
     }
 
@@ -102,7 +112,6 @@ public class signup extends AppCompatActivity implements View.OnClickListener{
                 EditText username = (EditText)findViewById(R.id.username);
                 EditText password = (EditText)findViewById(R.id.password);
                 EditText emails = (EditText)findViewById(R.id.email);
-                EditText DOB = (EditText)findViewById(R.id.DOB);
                 EditText city = (EditText)findViewById(R.id.city);
 
                 String user = username.getText().toString().trim();
@@ -111,8 +120,22 @@ public class signup extends AppCompatActivity implements View.OnClickListener{
                 String dob = DOB.getText().toString().trim();
                 String City = city.getText().toString().trim();
                 signup(email,pass,user,dob,City);
+            case R.id.DOB:
+                DialogFragment datePicker = new DatePickerFragment();
+                datePicker.show(getSupportFragmentManager(),"Date Picker");
             default:
                 return;
         }
+    }
+
+    @Override
+    public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+        Calendar calendar = Calendar.getInstance();
+        calendar.set(Calendar.YEAR, year);
+        calendar.set(Calendar.MONTH,month);
+        calendar.set(Calendar.DAY_OF_MONTH,dayOfMonth);
+        String cds = DateFormat.getDateInstance(DateFormat.FULL).format(calendar.getTime());
+        DOB.setText(cds);
+
     }
 }
